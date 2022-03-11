57d47d910d4b507b4eaa827a0bad84b7c635bb6f
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2022-03-11 17:36:10 -0600

    Add /public && styles

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
+app.use("/public", express.static("public"));
+
 app.get("/", async (req, res) => {
   const articles = await Article.find().sort({ createdAt: "desc" });
   res.render("articles/index", { articles: articles });
